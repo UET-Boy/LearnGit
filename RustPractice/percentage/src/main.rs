037c317adc9c85f8061fb528aba8810d7354b751
UET-Boy <Spellbreaker907@gmail.com>
2019-11-30 15:17:33 +0500

    Fsc Mat Perc Added

--- RustPractice/percentage/src/main.rs
+++ RustPractice/percentage/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let om: f32 = 90.0;
-    let tm: f32 = 150.0;
-    let div: f32 = om/tm;
-    let perc = div * 100.0; 
-    println!("Percentage = {}", perc);
+    let om_mat: f32 = 907.0;
+    let tm_mat: f32 = 1050.0;
+    let om_fsc: f32 = 918.0;
+    let tm_fsc: f32 = 1100.0;
+    let div_1: f32 = om_mat/tm_mat;
+    let div_2: f32 = om_fsc/tm_fsc;
+    let perc_mat = div_1 * 100.0;
+    let perc_fsc = div_2 * 100.0;
+    println!("Percentage (Matric) = {}", perc_mat);
+    println!("Percentage (F.Sc) = {}", perc_fsc);
 }
